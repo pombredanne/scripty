b3574daf9102005ccc21f12071f8f8b42ea6cb37
Dakshina Murthy <dkmurthy9@gmail.com>
2010-05-05 21:40:16 +0530

    Added mdb2sqlite

---
+++ mdb2sqlite.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -x
+
+if [ $# -ne 1 ]
+then
+  echo "Usage : $0 <mdb_file>"
+  exit 1
+fi
+
+mdb_file=$1
+sqlite_db=$1.sqlite
+temp_file=/tmp/$1.tmp
+
+# Create schema
+mdb-schema ${mdb_file} oracle | sqlite3 ${sqlite_db}
+
+for table in `mdb-tables ${mdb_file}`
+do
+
+echo Processing ${table}
+mdb-export -H -d "|||" ${mdb_file} ${table} >${temp_file}
+    
+sqlite3 ${sqlite_db} << _EOF_QUERY
+.separator "|||"
+.import ${temp_file} ${table}
+_EOF_QUERY
+
+done
