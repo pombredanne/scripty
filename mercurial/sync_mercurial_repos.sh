80a7f098f9ea35953a53454a2b607bddb4ee8c79
Dakshina Murthy <dkmurthy9@gmail.com>
2010-07-16 12:49:41 +0530

    Small cleanup to mercurial/sync_mercurial_repos.sh

--- mercurial/sync_mercurial_repos.sh
+++ mercurial/sync_mercurial_repos.sh
@@ -8,15 +8,17 @@ vidyalaya-config
 vidyalaya-active-directory
 "
 
+remote_url_base=ssh://hg@bitbucket.org/dkmurthy
+
 for repo in $repos
 do
     repo_dir="$HOME/scripts/${repo//-/_}"
     
     if [ ! -d $repo_dir ]
     then
-        hg clone http://bitbucket.org/dkmurthy/$repo $repo_dir
+        hg clone $remote_url_base/$repo $repo_dir
     fi
     
-    hg fetch -R $repo_dir ssh://hg@bitbucket.org/dkmurthy/$repo
-    hg push  -R $repo_dir ssh://hg@bitbucket.org/dkmurthy/$repo
+    hg fetch -R $repo_dir $remote_url_base/$repo
+    hg push  -R $repo_dir $remote_url_base/$repo
 done
