0411d22151660333287352d267882e0b546b0a5b
Dakshina Murthy <dkmurthy9@gmail.com>
2010-09-15 15:57:41 +0530

    Added script to backup to dropbox

---
+++ backup/upload_to_dropbox.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+while read source
+do
+    size=`sudo du -s $source |awk '{print $1}'`
+    if [[ -d "$source" && $size -lt 500000 ]]
+    then
+        echo Syncing $source
+        sudo rsync -az --delete $source /etc/dropbox/Dropbox/${source////_}/
+    else
+        echo Not processing $source
+    fi
+done < dropbox_list.txt
+
+sudo su -l dropbox -s /bin/bash -c "python /etc/dropbox/bin/dropbox.py start"
+
+sleep 1800
+
+sudo su -l dropbox -s /bin/bash -c "python /etc/dropbox/bin/dropbox.py stop"
+
