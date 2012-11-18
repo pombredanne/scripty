c1b67c45845da723fc0c2422faa973780c1001bb
Ramana <sramana9@gmail.com>
2012-11-18 11:22:07 +0530

    Adding script to change nice and ionice of common programs

---
+++ sysadmin/set_priorities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import psutil
+
+prio = {
+    'nginx': 'HIGH',
+    'mysqld': 'HIGH',
+    'uwsgi': 'HIGH',
+    'php-cgi': 'HIGH',
+
+    'cron': 'LOW',
+
+    'tor': 'VERY_LOW'
+}
+nice = dict(HIGH=-10, LOW=15, VERY_LOW=19)  # -20 to 19
+ionice = dict(HIGH=0, LOW=5, VERY_LOW=7)    # 0 to 7
+
+processes = [p for p in psutil.process_iter() if p.name in prio]
+
+for p in processes:
+    old_nice = p.get_nice()
+    old_ionice = p.get_ionice().value
+    priority = prio[p.name]
+    p.set_nice(nice[priority])
+    p.set_ionice(psutil.IOPRIO_CLASS_BE, ionice[priority])
+
+    print "%s \n old_nice=%s, new_nice=%s \n old_ionice=%s, new_ionice=%s" % (
+        p,
+        old_nice, p.get_nice(),
+        old_ionice, p.get_ionice().value)
+
