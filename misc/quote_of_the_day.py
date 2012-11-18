9f805a91c9a0084a1586a70baf8366a3e2b62d9c
Ramana <sramana9@gmail.com>
2012-11-18 10:53:12 +0530

    Using Tumblr's new API for quote_of_the_day.py

--- misc/quote_of_the_day.py
+++ misc/quote_of_the_day.py
@@ -4,19 +4,21 @@ import os
 import requests
 from BeautifulSoup import BeautifulSoup
 from logbook import debug, info, warn, error
-import tumblr
 import cofre
+from tumblpy import Tumblpy
 
 url = 'http://www.vivekananda.org'
-blog = 'swamiji.tumblr.com'
+blog_url = 'swamiji.tumblr.com'
 
-username = cofre.get('tumblr_username')
-password = cofre.get('tumblr_password')
+t = Tumblpy(app_key=cofre.get('tumblr_app_key'),
+            app_secret=cofre.get('tumblr_app_secret'),
+            oauth_token=cofre.get('tumblr_oauth_token'),
+            oauth_token_secret=cofre.get('tumblr_oauth_token_secret'))
 
 
 def fetch_quote():
     debug("Fetching the quote")
-    html = requests.get(url).read()
+    html = requests.get(url).text
 
     soup = BeautifulSoup(html)
     # The quote is enclosed in 'p' tag
@@ -31,8 +33,12 @@ def post_to_tumblr(quote):
     if 'Microsoft' in quote:
         return
     debug("Publishing to Tumblr")
-    api = tumblr.Api(blog, username, password)
-    api.write_quote(quote=quote, source="Swami Vivekananda")
+    t.post('post',
+           blog_url=blog_url,
+           params=dict(type='quote',
+                       quote=quote,
+                       source='Swami Vivekananda')
+          )
 
 
 def main():
